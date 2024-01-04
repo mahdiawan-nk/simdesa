3cbf3715349678b7234189abb1c19926dc4caf1c
mahdiawan-nk <dev.mrcoders@gmail.com>
2024-01-04 22:28:12 +0700

    update

--- application/views/_partials/sites/footer.php
+++ application/views/_partials/sites/footer.php
@@ -58,7 +58,7 @@
                     <div class="footer-copy-right">
                         <p>Copyright &copy;<script>
                                 document.write(new Date().getFullYear());
-                            </script> All rights reserved | This template is made with Testing <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            </script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
                     </div>
                 </div>
             </div>
