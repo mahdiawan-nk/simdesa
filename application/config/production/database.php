f14ba3d53b687958a1e65f32b20d11ef67acca7f
mahdiawan-nk <dev.mrcoders@gmail.com>
2024-01-11 04:46:54 +0700

    update config env

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'app-desa',
+	'username' => 'mhsm8239_tara',
+	'password' => 'tara@2023',
+	'database' => 'mhsm8239_tara',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
